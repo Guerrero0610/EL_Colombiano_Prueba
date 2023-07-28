4bdb2f6884c1891535ac610a28a92672f332094f
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-07-28 16:32:04 -0500

    Regulaciones

--- Pruebas/CRUD_El_Colombiano/CRUD_El_Colombiano/Models/PersonasInteresada.cs
+++ Pruebas/CRUD_El_Colombiano/CRUD_El_Colombiano/Models/PersonasInteresada.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CRUD_El_Colombiano.Models;
 
@@ -7,16 +8,23 @@ public partial class PersonasInteresada
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
     public string Nombre { get; set; } = null!;
 
+    [Required(ErrorMessage = "El campo Apellido es obligatorio.")]
     public string Apellido { get; set; } = null!;
 
+    [Required(ErrorMessage = "El campo Correo es obligatorio.")]
     public string CorreoElectronico { get; set; } = null!;
 
+    [Required(ErrorMessage = "El campo Numero de Telefono es obligatorio.")]
+    [RegularExpression(@"^\d+$", ErrorMessage = "El campo Numero de Telefono solo debe contener números.")]
     public string NumeroTelefono { get; set; } = null!;
 
+    [Required(ErrorMessage = "El campo Proyecto de Interes es obligatorio.")]
     public int? ProyectoDeInteres { get; set; }
 
+    [Required(ErrorMessage = "El campo Fecha de Registro es obligatorio.")]
     public DateTime FechaRegistro { get; set; }
 
     public virtual Proyecto? ProyectoDeInteresNavigation { get; set; }
