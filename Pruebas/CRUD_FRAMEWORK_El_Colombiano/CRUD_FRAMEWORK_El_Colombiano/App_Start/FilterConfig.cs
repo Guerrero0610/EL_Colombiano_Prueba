18687634d4076f6c0b7b352d845a90b4e0cd58c1
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-07-27 19:34:16 -0500

    CAMBIOS

---
+++ Pruebas/CRUD_FRAMEWORK_El_Colombiano/CRUD_FRAMEWORK_El_Colombiano/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_FRAMEWORK_El_Colombiano
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Pruebas/CRUD_FRAMEWORK_El_Colombiano/CRUD_FRAMEWORK_El_Colombiano/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRUD_FRAMEWORK_El_Colombiano.MvcApplication" Language="C#" %>
