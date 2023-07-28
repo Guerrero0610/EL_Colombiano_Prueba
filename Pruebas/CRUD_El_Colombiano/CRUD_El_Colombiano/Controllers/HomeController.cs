408a948588aed7d73446e658174a7b72efb531ac
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-07-27 19:36:17 -0500

    EL COLOMBIANO PRUEBA

---
+++ Pruebas/CRUD_El_Colombiano/CRUD_El_Colombiano/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CRUD_El_Colombiano.Models;
+using CRUD_El_Colombiano.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CRUD_El_Colombiano.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BdproyectosContext _DBContext;
+
+        public HomeController(BdproyectosContext context)
+        {
+            _DBContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<PersonasInteresada> Lista = _DBContext.PersonasInteresadas.Include(c => c.ProyectoDeInteresNavigation).ToList();
+            return View(Lista);
+        }
+
+        [HttpGet]
+        public IActionResult PersonaInteDetalle()
+        {
+            ClienteVM oPersonaVM = new ClienteVM(){
+                oPersona = new PersonasInteresada(),
+                oLista = _DBContext.Proyectos.Select(proyec => new SelectListItem()
+                {
+                    Text = proyec.Constructora,
+                    Value = proyec.Codigo.ToString()
+
+                }).ToList()
+
+            };
+
+
+             return View(oPersonaVM);
+        }
+
+        [HttpPost]
+        public IActionResult PersonaInteDetalle(ClienteVM oPersonaVM)
+        {
+            if(oPersonaVM.oPersona.Id == 0)
+            {
+                _DBContext.PersonasInteresadas.Add(oPersonaVM.oPersona);
+            }
+
+            _DBContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Pruebas/CRUD_El_Colombiano/CRUD_El_Colombiano/Models/ViewModels/ClienteVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CRUD_El_Colombiano.Models.ViewModels
+{
+    public class ClienteVM
+    {
+        public PersonasInteresada oPersona { get; set; } 
+
+        public List<SelectListItem> oLista { get; set; }
+    } 
+}
