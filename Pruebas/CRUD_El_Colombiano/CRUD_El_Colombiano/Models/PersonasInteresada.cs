04957bef2997fe629cc1ff48ccc710246d670d98
Anggie Guerrero <90532252+Guerrero0610@users.noreply.github.com>
2023-07-27 19:28:32 -0500

    PASANDO PRUEBA A GITHUB
    Copia Respaldo

---
+++ Pruebas/CRUD_El_Colombiano/CRUD_El_Colombiano/Models/PersonasInteresada.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUD_El_Colombiano.Models;
+
+public partial class PersonasInteresada
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string CorreoElectronico { get; set; } = null!;
+
+    public string NumeroTelefono { get; set; } = null!;
+
+    public int? ProyectoDeInteres { get; set; }
+
+    public virtual Proyecto? ProyectoDeInteresNavigation { get; set; }
+}
